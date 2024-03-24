d4004edade6a0e48c3bf1eb75a93bdd77ec4ebb0
Amaalalhasan0 <amaalalhasan0@gmail.com>
2024-03-24 23:16:57 +0300

    Log in

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import'../src/assets/Icons/fontawesome-free-6.5.1-web/css/all.css'
+import './App.css';
+import MyNavbar from './components/MyNavbar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <MyNavbar/>
+    </div>
+  );
+}
+
+export default App;
